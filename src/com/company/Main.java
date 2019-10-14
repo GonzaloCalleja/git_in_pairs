e2c10bd24e743526ccfb59ac52dac6c03aeb5484
GonzaloCalleja <37070611+GonzaloCalleja@users.noreply.github.com>
2019-10-14 11:48:30 +0200

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world, Gonzalo's fork");
 
-	System.out.println("Julian has infested the system o_o")
+	System.out.println("Julian has infested the system o_o, but has been detected and stopped. :)")
 
 	System.out.println("Hello Gon, I'm Clara's spoon. I fork your project.");
    }
